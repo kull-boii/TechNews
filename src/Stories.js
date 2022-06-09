8a09df6f7a4f9511b64bd446b487f452f9813537
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-06-09 16:30:26 +0530

    fecth data

--- src/Stories.js
+++ src/Stories.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Stories = () => {
-  return <div>Stories</div>;
+  let isLoading = true;
+
+  let API = "http://hn.algolia.com/api/v1/search?query=html";
+
+  const fetchAPiData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      //   isLoading = false;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAPiData(API);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <h1>Loading....</h1>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>My tech news Post</h2>
+    </>
+  );
 };
 
 export default Stories;
